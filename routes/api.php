0d673709e949cfecf99490f38cbe3a241e9ebb5c
alvi <mohalviridho@gmail.com>
2025-02-27 11:11:48 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 // Route::apiResource('products', ProductController::class);
@@ -23,7 +23,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('products', ProductController::class);
     Route::post('/orders', [OrderController::class, 'store']);
     Route::put('/orders/{id}/status', [OrderController::class, 'updateStatus']);
-    // Route::post('/invoices/{orderId}', [InvoiceController::class, 'generateInvoice']);
     Route::post('/invoices/{orderId}', [InvoiceController::class, 'generateInvoice']);
     Route::get('/', [InvoiceController::class, 'index']);
     Route::get('/{id}', [InvoiceController::class, 'show']);
